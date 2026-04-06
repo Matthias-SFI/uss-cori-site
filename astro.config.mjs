19b2ac579e99efb9c2928f73303d779d1d2d41f7
Matthias-SFI <bm751270@gmail.com>
2026-04-06 11:32:24 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://matthias-sfi.github.io',
+    base: '/uss-cori-site',
+});
